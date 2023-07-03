25ff6b5aba311adb2d142f73c670f4b7fdbf0620
wengxiulin <2304658432@qq.com>
2023-07-03 19:55:33 +0800

    codingmore-logback

--- codingmore-logback/src/main/java/top/codingmore/CodingmoreLogbackApplication.java
+++ codingmore-logback/src/main/java/top/codingmore/CodingmoreLogbackApplication.java
@@ -3,10 +3,18 @@ package top.codingmore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 日志启动类
+ */
 @SpringBootApplication
 public class CodingmoreLogbackApplication {
 
+	/**
+	 * 启动方法
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// 启动
 		SpringApplication.run(CodingmoreLogbackApplication.class, args);
 	}
 
