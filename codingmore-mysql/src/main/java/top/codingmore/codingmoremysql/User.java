54db5d97ed4396f421da14a75edc948231ac7a04
wengxiulin <2304658432@qq.com>
2023-07-04 20:25:33 +0800

    codingmore-mysql

--- codingmore-mysql/src/main/java/top/codingmore/codingmoremysql/User.java
+++ codingmore-mysql/src/main/java/top/codingmore/codingmoremysql/User.java
@@ -3,6 +3,9 @@ package top.codingmore.codingmoremysql;
 import lombok.Data;
 
 /**
+ * <p>
+ *     用户表
+ * </p>
  * 微信搜索「沉默王二」，回复 Java
  *
  * @author 沉默王二
@@ -10,8 +13,16 @@ import lombok.Data;
  */
 @Data
 public class User {
+
+    // id
     private Integer id;
+
+    // 名称
     private String name;
+
+    // 密码
     private String password;
+
+    // 年龄
     private Integer age;
 }
